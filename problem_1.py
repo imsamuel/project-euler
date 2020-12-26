a2e8b03e3eb77530a8a337486e16c47807865ecf
imsamuel <samuelywp@gmail.com>
2020-12-26 23:23:35 +0800

    feat: add problem 1 solution

---
+++ problem_1.py
@@ -0,0 +1,19 @@
+"""Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 and 5, we
+get 3, 5, 6, and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 100.
+"""
+
+
+def solution():
+    sum = 0
+    for n in range(1, 100):
+        if n % 3 == 0 or n % 5 == 0:
+            sum += n
+    print(sum)
+
+
+solution()
+
